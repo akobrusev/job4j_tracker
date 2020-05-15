457fc383850ba3d78c1a8e3ef43f986d30869427
akobrusev <akobrusev@gmail.com>
2020-05-15 21:49:58 +0300

    1.7. Состояние объекта. [#267378]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,14 +8,14 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
+        this.load = this.load - another.load;
         another.load = 0;
     }
 
     public static void main(String[] args) {
         Battery charge = new Battery(100);
-        Battery discharge1 = new Battery(-5);
-        Battery discharge2 = new Battery(-25);
+        Battery discharge1 = new Battery(5);
+        Battery discharge2 = new Battery(25);
         System.out.println("charge : " + charge.load + ". discharge1 : " + discharge1.load);
         charge.exchange(discharge1);
         System.out.println("charge : " + charge.load + ". discharge2 : " + discharge2.load);
