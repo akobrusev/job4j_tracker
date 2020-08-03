6e351b6ab8b25fce4e084a02fb77bcf6700873b9
akobrusev <akobrusev@gmail.com>
2020-08-03 18:08:29 +0300

    5. Реализовать класс Tracker[396#267397]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,58 @@
 package ru.job4j.tracker;
-
+import java.util.Arrays;
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+
     public Item add(Item item) {
-        item.setId(generateId());
+        item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    private String generateId() {
-        return String.valueOf(ids++);
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemsWithoutNull[size] = item;
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        for (int index = 0; index < itemsWithoutNull.length; index++) {
+            System.out.println(itemsWithoutNull[index]);
+        }
+        return items;
     }
 
-    public Item findById(String id) {
+    public Object findByName(String key) {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                if(item.getName().equals(key)) {
+                    itemsWithoutNull[size] = item;
+                    size++;
+                }
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        for (int index = 0; index < itemsWithoutNull.length; index++) {
+            System.out.println(itemsWithoutNull[index]);
+        }
+        return items;
+    }
+
+    public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if (item.getId().equals(id)) {
+            if (item.getId() == id) {
                 rsl = item;
                 break;
             }
