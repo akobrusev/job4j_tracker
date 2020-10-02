7375bb33ba8c69823f56065535410bce7453586b
akobrusev <akobrusev@gmail.com>
2020-10-02 08:30:49 +0300

    2. Scanner и чтение числа из консоли.[179518#267333]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean run = true;
         int matches = 11;
         int result;
-        while (matches > 0) {
-            System.out.println("Игра 11");
-            System.out.println("Можно взять 1, 2 или 3 спички");
-            System.out.print("Введите колличество спичек, чтобы начать игру: ");
-            int select = Integer.valueOf(input.nextLine());
-            if (select == 3) {
-              result = matches - 3;
-                System.out.println("Игра игра продолжается осталось спичек " + result;
+        System.out.println("Игра 11");
+        System.out.println("Можно взять 1, 2 или 3 спички");
 
-            } else if (select == 1) {
-                result = matches - 1;
-                System.out.println("Игра игра продолжается осталось спичек " + result);
-            } else if (select == 2) {
-                result = matches - 2;
-                System.out.println("Игра игра продолжается осталось спичек " + result);
+            while (run) {
+                System.out.println(matches + " спичек осталось" );
+                System.out.println("Введите колличество спичек, чтобы начать игру: ");
+            int select = Integer.valueOf(input.nextLine());
+            if (select < 1 || select > 3) {
+                System.out.println("Можно брать от 1 до 3 спичек.");
             } else {
-                System.out.println("Можно юрать от 1 до 3 спичек.");
+                result = matches - select;
+                if (result < 0) {
+                    System.out.println("На столе осталось меньше спичек, попробуй снова");
+                } else {
+                    matches = result;
+                }
             }
-            System.out.println();
+            if (matches == 0) {
+                run = false;
+                System.out.println("Игра окончена");
+            }
         }
+
     }
 }
