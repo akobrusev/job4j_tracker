354a66591461685eb6094f8f89aded4f9499beb1
Anton <akobrsev@gmail.com>
2021-03-01 21:47:05 +0300

    9.1 Рефакторинг теста @Before @After [#267330]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,52 +8,20 @@ import static org.junit.Assert.assertThat;
 public class StartUITest {
 
     @Test
-    public void whenCreateAction() {
+    public void whenExit() {
+        Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[] {"0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-
                 new Exit()
         };
-
-        assertThat(tracker.findAll()[0].getName(), is("Item name"));
-    }
-
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
-        Item item = tracker.add(new Item("Replaced item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
-        String replacedName = "New item name";
-        Input in = new StubInput(
-                new String[] {"0","1","New item name", "1"}
-        );
-        UserAction[] actions = {
-
-                new Exit()
-        };
-
-        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
-    }
-    @Test
-    public void whenDeleteAction() {
-        Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
-        Item item = tracker.add(new Item("Deleted item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
-        Input in = new StubInput(
-                new String[] {"0","Dimon","1","1","2"}
-        );
-        UserAction[] actions = {
-
-
-                new Exit()
-        };
-
-        assertThat(tracker.findById(item.getId()), is(nullValue()));
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Exit program" + System.lineSeparator()
+        ));
     }
 
 }
